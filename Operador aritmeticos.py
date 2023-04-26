184783d8e85e2918625aa7c64094768db6d2214a
Marco Antonio Ortiz Ramirez <131299835+Marko24K@users.noreply.github.com>
2023-04-26 15:41:41 -0400

    Clase 26/04/2023 periodo1

---
+++ Operador aritmeticos.py
@@ -0,0 +1,24 @@
+print("Suma")
+print(2 + 2)
+
+print("Resta")
+print(5 - 8)
+
+print("multiplicacion")
+print(4 * 7)
+
+print("Division")
+print(15 / 3)
+
+print("Modulo")
+print(7 % 9)
+
+print("Exponente")
+print(2 ** 4)
+
+print("Orden de predencia para operaciones aritmeticas")
+print("parentesis, potencias, raices, multiplicacion, division, suma y la resta ")
+
+print(5 + 5 * 2)
+#investigar division entera
+#y hacer raices cuadradadas
---
+++ variable.py
@@ -0,0 +1,16 @@
+num = 3
+print(num)
+
+numero1 = 54
+numero2 = 32
+print(numero1+numero2)
+
+resultado=numero1+numero2
+print(resultado)
+
+#Reglas del nombre de las variables 
+#1. no pueden empezar por un numero
+#2. siempre sean en minusculas
+#3. separar las palabras por un _
+#4. no pueden contenner caracteres especiales a excepcion del _
+
